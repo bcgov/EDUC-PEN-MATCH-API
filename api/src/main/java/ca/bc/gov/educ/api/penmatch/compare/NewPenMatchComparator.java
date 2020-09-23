fe48addca439313c631e0fe10c3bb2cc9499a406
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2020-09-23 10:39:24 -0700

    Modify PEN Match comparator to push '?' PENs to the bottom of the list and some logging for method execution times.

--- api/src/main/java/ca/bc/gov/educ/api/penmatch/compare/NewPenMatchComparator.java
+++ api/src/main/java/ca/bc/gov/educ/api/penmatch/compare/NewPenMatchComparator.java
@@ -5,16 +5,16 @@ import ca.bc.gov.educ.api.penmatch.struct.v1.newmatch.BestMatchRecord;
 import java.util.Comparator;
 
 public class NewPenMatchComparator implements Comparator<BestMatchRecord> {
-    @Override
-    public int compare(BestMatchRecord x, BestMatchRecord y) {
-        //This is a single result situation...no algorithm was used
-        if(x.getMatchValue() == null){
-            return 0;
-        }
-        if (x.getMatchValue() < y.getMatchValue()) {
-            return -1;
-        }
-
-        return 0;
+  @Override
+  public int compare(BestMatchRecord x, BestMatchRecord y) {
+    //This is a single result situation...no algorithm was used
+    if (x.getMatchValue() == null) {
+      return 0;
     }
+    if (x.getMatchValue() < y.getMatchValue() || (x.getMatchPEN() != null && x.getMatchPEN().contains("?"))) {
+      return -1;
+    }
+
+    return 0;
+  }
 }
