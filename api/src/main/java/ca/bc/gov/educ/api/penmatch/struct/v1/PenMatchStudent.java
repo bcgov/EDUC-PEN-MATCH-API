903279f30070eaae214a6e07320382ef58211381
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2020-10-05 16:46:31 -0700

    once char given name or surname not allowed...

--- api/src/main/java/ca/bc/gov/educ/api/penmatch/struct/v1/PenMatchStudent.java
+++ api/src/main/java/ca/bc/gov/educ/api/penmatch/struct/v1/PenMatchStudent.java
@@ -10,28 +10,28 @@ import javax.validation.constraints.Size;
 @Data
 @NoArgsConstructor
 public class PenMatchStudent {
-	protected String pen;
-	@Pattern(regexp = "^((19|20)\\d\\d)(0[1-9]|1[012])(0[1-9]|[12][0-9]|3[01])$")
-	@NotNull(message = "Date of Birth can not be null.")
-	protected String dob;
-	@NotNull(message = "Sex Code can not be null.")
-	protected String sex;
-	protected String enrolledGradeCode;
-	@Size(max = 255)
-	@NotNull(message = "Surname can not be null.")
-	protected String surname;
-	@Size(max = 255)
-	@NotNull(message = "Given name can not be null.")
-	protected String givenName;
-	protected String middleName;
-	protected String usualSurname;
-	protected String usualGivenName;
-	protected String usualMiddleName;
-	protected String mincode;
-	protected String localID;
-	protected String postal;
-	protected String updateCode;
-	protected String applicationCode;
-	protected boolean assignNewPEN;
+  protected String pen;
+  @Pattern(regexp = "^((19|20)\\d\\d)(0[1-9]|1[012])(0[1-9]|[12][0-9]|3[01])$")
+  @NotNull(message = "Date of Birth can not be null.")
+  protected String dob;
+  @NotNull(message = "Sex Code can not be null.")
+  protected String sex;
+  protected String enrolledGradeCode;
+  @Size(min = 2, max = 255)
+  @NotNull(message = "Surname can not be null.")
+  protected String surname;
+  @Size(min = 2, max = 255)
+  @NotNull(message = "Given name can not be null.")
+  protected String givenName;
+  protected String middleName;
+  protected String usualSurname;
+  protected String usualGivenName;
+  protected String usualMiddleName;
+  protected String mincode;
+  protected String localID;
+  protected String postal;
+  protected String updateCode;
+  protected String applicationCode;
+  protected boolean assignNewPEN;
 
 }
