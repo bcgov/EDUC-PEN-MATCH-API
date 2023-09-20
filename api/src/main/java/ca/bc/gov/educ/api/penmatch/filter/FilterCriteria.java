27dfdc4e7d13a375d34bb646edfe01aa518041ed
SoLetsDev <74216496+SoLetsDev@users.noreply.github.com>
2023-09-20 10:44:41 -0700

    removing unused imports

--- api/src/main/java/ca/bc/gov/educ/api/penmatch/filter/FilterCriteria.java
+++ api/src/main/java/ca/bc/gov/educ/api/penmatch/filter/FilterCriteria.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 /**
  * Filter Criteria Holder
